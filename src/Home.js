edc7b6a392d88006c17b2ddfa341bc16b2199ac6
Badal Kumar Behera <surajbadal69@gmail.com>
2023-02-10 15:21:22 +0530

    All files are commit to the repository

---
+++ src/Home.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+       <div class="container-fluid">
+    <a class="navbar-brand" href="/">PUBLICATION MANAGEMENT SYSTEM</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <Link to="/login" class="nav-link active" aria-current="page">LOGIN</Link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="null">REGISTER</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="null">FACULTY SYSTEM</a>
+        </li>
+        <li class="nav-item dropdown dropstart">
+          <a class="nav-link dropdown-toggle" href="null" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            OTHERS
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="null">Publication</a></li>
+            <li><a class="dropdown-item" href="null">Projects</a></li>
+            <li><a class="dropdown-item" href="null">Products</a></li>
+            <li><a class="dropdown-item" href="null">Awards</a></li>
+            <li><a class="dropdown-item" href="null">Research Scholar Guidance</a></li>
+            <li><a class="dropdown-item" href="null">Learning Material Developed</a></li>
+            <li><a class="dropdown-item" href="null">FDP</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+  </nav>
+      </div>
+    
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+import Home from './Home';
+import "./Home.css"
+import { Login } from './pages/Login';
+import "./pages/Login.css" ;
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+
+const router = createBrowserRouter([
+   {
+     path: "/",
+     element: <Home/>,
+   },
+   {
+      path: "login",
+      element: <Login/>,
+    },
+ ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  
+   <React.StrictMode>
+   <RouterProvider router={router} />
+ </React.StrictMode> 
+   
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
