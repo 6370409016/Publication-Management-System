10d5151d4dacafd5c84b28e7de9f0ff29906cde1
Badal Kumar Behera <surajbadal69@gmail.com>
2023-02-17 11:00:35 +0530

    modify Home.css and Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,42 +4,45 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-       <div class="container-fluid">
-    <Link to="/" class="navbar-brand" >PUBLICATION MANAGEMENT SYSTEM</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <Link to="/login" class="nav-link active" aria-current="page">LOGIN</Link>
-        </li>
-        <li class="nav-item">
-        <Link to="/register" class="nav-link active" aria-current="page">REGISTER</Link>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="null">FACULTY SYSTEM</a>
-        </li>
-        <li class="nav-item dropdown dropstart">
-          <a class="nav-link dropdown-toggle" href="null" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            OTHERS
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="null">Publication</a></li>
-            <li><a class="dropdown-item" href="null">Projects</a></li>
-            <li><a class="dropdown-item" href="null">Products</a></li>
-            <li><a class="dropdown-item" href="null">Awards</a></li>
-            <li><a class="dropdown-item" href="null">Research Scholar Guidance</a></li>
-            <li><a class="dropdown-item" href="null">Learning Material Developed</a></li>
-            <li><a class="dropdown-item" href="null">FDP</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-  </nav>
+      <nav class="  navbar navbar-expand-lg ">
+        <div class="container-fluid">
+          <Link to="/" class="navbar-brand" >PUBLICATION MANAGEMENT SYSTEM</Link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <Link to="/login" class="nav-link active" aria-current="page">LOGIN</Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/register" class="nav-link active" aria-current="page">REGISTER</Link>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="null">FACULTY SYSTEM</a>
+              </li>
+              <li class="nav-item dropdown dropstart">
+                <a class="nav-link dropdown-toggle" href="null" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  OTHERS
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="null">Publication</a></li>
+                  <li><a class="dropdown-item" href="null">Projects</a></li>
+                  <li><a class="dropdown-item" href="null">Products</a></li>
+                  <li><a class="dropdown-item" href="null">Awards</a></li>
+                  <li><a class="dropdown-item" href="null">Research Scholar Guidance</a></li>
+                  <li><a class="dropdown-item" href="null">Learning Material Developed</a></li>
+                  <li><a class="dropdown-item" href="null">FDP</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div class="middile-container">
+          <h1 class="middile-headind">CENTURION UNIVERSITY OF TECHNOLOGY AND MANAGEMENT</h1>
       </div>
-    
+    </div>
+
   )
 }
